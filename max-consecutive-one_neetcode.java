56a54ebbe52644ade300bead145cc2ec49a32521
namvh <hoangnam312.dev@gmail.com>
2026-06-17 16:00:41 +0700

    Refactor findMaxConsecutiveOnes method and add a new file for the solution

--- max-consecutive-one_neetcode.java
+++ max-consecutive-one_neetcode.java
@@ -15,15 +15,3 @@ class Solution {
         return maxCount;
     }
 }
-
-class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        int[] nums = {1,1,0,1,1,1};
-
-        System.out.println(
-            solution.findMaxConsecutiveOnes(nums)
-        );
-    }
-}
