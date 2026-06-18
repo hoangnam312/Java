6b3a84b287d0674cc8252e54dc398cb2533301c1
namvh <hoangnam312.dev@gmail.com>
2026-06-18 18:14:48 +0700

    reverse linked list

---
+++ neetcode/DSA for beginers/5. Linked List/Reverse Linked List/local.java
@@ -0,0 +1,81 @@
+// Reverse Linked List
+// Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+
+// Example 1:
+// Input: head = [0,1,2,3]
+// Output: [3,2,1,0]
+
+// Example 2:
+// Input: head = []
+// Output: []
+
+// Constraints:
+// 0 <= The length of the list <= 1000.
+// -1000 <= Node.val <= 1000
+
+
+class Main {
+    public static void main(String[] args) {
+        // Tạo linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original: ");
+        printList(head);
+
+        ListNode reversed = reverseList(head);
+
+        System.out.print("Reversed: ");
+        printList(reversed);
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = head;
+        int i = 0;
+        while (next != null && i < 10) {
+            System.out.println("------------- " + i);
+            printList(head);
+            // prev = next;
+            next = next.next;
+            prev = head;
+            head.next = prev;
+            i = i + 1;
+        }
+
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+
+        while (current != null) {
+            System.out.print(current.val);
+
+            if (current.next != null) {
+                System.out.print(" -> ");
+            }
+
+            current = current.next;
+        }
+
+        System.out.println();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
